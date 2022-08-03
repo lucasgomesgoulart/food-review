23f2a1a41dad7295292f5c32718ca08154e071ec
Eduardo Paixão Sehn <78269439+Eduardo668@users.noreply.github.com>
2022-08-02 22:14:44 -0300

    conection db

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 import express from "express";
+import dotenvn from "dotenv"
+
+dotenvn.config({path:'./config/config.env'})
+const PORT = process.env.PORT;
+
 
 const server = express();
-server.listen(3000, console.log("servidor rodando"));
+server.listen(PORT, console.log("Servidor rodando em: " + process.env.NODE_ENV + ","+ " na Porta: " + PORT));
 
 server.get('/' ,(req, res) => res.send('falaMarquinhos'))
-server.get('/users',(req,res)=> res.send('pagina de usuarios'))
+server.get('/users',(req,res)=> res.send('pagina de usuariosssss'))
+
+
+// CONEXÃO COM BANCO DE DADOS
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'foodReview',
+  password : 'foodReview',
+  database : 'dbFoodReview'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
